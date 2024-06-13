8f4f0f26bf6c2496d38ec06cbe4253541f13a0b7
Pierre Guillemot <pierregllmt@gmail.com>
2024-06-13 15:00:04 +0200

    accesibility form ok

--- src/components/FormWithoutReactHookForm.tsx
+++ src/components/FormWithoutReactHookForm.tsx
@@ -44,47 +44,88 @@ function FormWithoutReactHookForm() {
       {errors.length > 0 && (
         <div className="flex flex-col gap-y-2 bg-red-100 p-4 rounded-md">
           {errors.map((error) => (
-            <p key={error} className="text-red-500">
+            <p role="alert" key={error} className="text-red-500">
               {error}
             </p>
           ))}
         </div>
       )}
+
+      <label htmlFor="email">
+        Email{" "}
+        <span className="text-red-500" aria-hidden="true">
+          *
+        </span>
+      </label>
       <input
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         type="email"
         required
+        aria-required="true"
+        aria-invalid={true}
         maxLength={50}
         name="email"
         id="email"
         placeholder="Email"
         className="px-4 py-2 rounded text-black"
+        aria-describedby="emailAccessibleInfos"
       />
+      <span id="emailAccessibleInfos" className="sr-only">
+        Email is required
+      </span>
+
+      <label htmlFor="password">
+        Password{" "}
+        <span className="text-red-500" aria-hidden="true">
+          *
+        </span>
+      </label>
       <input
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         type="password"
         required
+        aria-required="true"
+        aria-invalid={true}
         minLength={8}
         maxLength={50}
         name="password"
         id="password"
         placeholder="Password"
         className="px-4 py-2 rounded text-black"
+        aria-describedby="passwordAccessibleInfos"
       />
+      <span id="passwordAccessibleInfos" className="sr-only">
+        Password must be at least 8 characters long and maximum 50 characters
+      </span>
+
+      <label htmlFor="confirmPassword">
+        Confirm Password{" "}
+        <span className="text-red-500" aria-hidden="true">
+          *
+        </span>
+      </label>
       <input
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)}
         type="password"
         required
+        aria-required="true"
+        aria-invalid={password !== confirmPassword}
         minLength={8}
         maxLength={50}
         name="confirmPassword"
         id="confirmPassword"
         placeholder="Confirm Password"
         className="px-4 py-2 rounded text-black"
+        aria-describedby="confirmPasswordAccessibleInfos"
       />
+      <span id="confirmPasswordAccessibleInfos" className="sr-only">
+        Confirm Password must be at least 8 characters long and maximum 50
+        characters
+      </span>
+
       <button
         className="px-4 py-2 bg-blue-800 text-white disabled:bg-gray-500 rounded group flex items-center justify-center"
         disabled={isSubmitting}
